2430f760e922deb8285f10a9c4c5dff772606392
Mike Holm <mike@mike-HP-Pavilion-dv9500-Notebook-PC.(none)>
2013-04-21 15:06:47 -0500

    get the pen drawing image size just right

--- banner.php
+++ banner.php
@@ -1,2 +1,2 @@
-<img class="banner" src="images/pen_ladyslipper_spgc_106x200.jpg" alt="Pen&Ink drawing of a Ladyslipper" />
+<img class="banner" src="images/pen_ladyslipper_spgc_133x250.jpg" alt="Pen&Ink drawing of a Ladyslipper" />
 <h1 class="banner">Saint Paul Garden Club</h1>
