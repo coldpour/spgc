edd563d6461638f3383ef8f1adad33746ace79eb
Mike Holm <mholm815@gmail.com>
2013-04-21 16:08:59 -0500

    email input type should be email

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <form class="login" action="welcome.php" method="post">
-      <input class="login" type="text" placeholder="email address">
+      <input class="login" type="email" placeholder="email address">
       <input class="login" type="password" placeholder="password">
       <input class="login" type="submit" value="Log In">
 </form>
