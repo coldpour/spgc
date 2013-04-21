da988384292ef4680f7d6737007bb1acee863eb7
Mike Holm <mholm815@gmail.com>
2013-04-21 17:37:21 -0500

    get broken line and middle stripe working behind the slide img

--- index.php
+++ index.php
@@ -12,9 +12,11 @@
      include 'banner.php';
      ?>
 
-  <div class="center slide">
-    <?php include 'nav.php';?>
-    <img class="slide" src="images/lady_slipper_USFWS.jpg" />
+  <div class="stripe">
+    <div class="center slide">
+      <?php include 'nav.php';?>
+      <img class="slide" src="images/lady_slipper_USFWS.jpg" />
+    </div>
   </div>
 </div>
 </body>
