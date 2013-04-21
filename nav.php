d040075bc42c1d928189bcbdd845d6d684e2aabc
Mike Holm <mholm815@gmail.com>
2013-04-21 16:23:02 -0500

    add links

---
+++ nav.php
@@ -0,0 +1,5 @@
+<div class="nav">
+        <a id="about" class="nav" href="about.php">ABOUT</a> |
+        <a class="nav" href="event.php">PROJECTS</a> |
+        <a class="nav" href="donate.php">LINKS</a>
+</div>
