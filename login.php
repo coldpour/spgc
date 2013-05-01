fb46ceaa516297fe2e89d1f1e2fea70fc5035a64
Mike Holm <mholm815@gmail.com>
2013-05-01 00:32:31 -0500

    start work on the login feature

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <form class="login" action="welcome.php" method="post">
-      <input class="login" type="email" placeholder="email address">
-      <input class="login" type="password" placeholder="password">
+      <input class="login" type="email" placeholder="email address" name="user">
+      <input class="login" type="password" placeholder="password" name="pword">
       <input class="login" type="submit" value="Log In">
 </form>
