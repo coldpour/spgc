fe6c3ac4738c9453559d63f8a77d433326e9bac6
Mike Holm <mholm815@gmail.com>
2013-05-01 01:40:48 -0500

    get password hashing working

--- welcome.php
+++ welcome.php
@@ -1,3 +1,13 @@
 <?php
-echo crypt($_POST["pword"]);
+require ("passhash.php");
+
+$pass_hash = PassHash::hash($_POST['pword']);
+echo $pass_hash;
+
+if (PassHash::check_password($pass_hash, $_POST['pword'])) {
+   echo "Access Granted!";
+} else {
+   echo "Access Denied";
+}
+
 ?>
