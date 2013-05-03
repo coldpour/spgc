8afcb11660f845c5663829b0cb60f73644734c65
Mike Holm <mholm815@gmail.com>
2013-05-02 23:32:34 -0500

    remove email from login. going to have a single password for version 1

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <form class="login" action="welcome.php" method="post">
-      <input class="login" type="email" placeholder="email address" name="user">
       <input class="login" type="password" placeholder="password" name="pword">
       <input class="login" type="submit" value="Log In">
 </form>
