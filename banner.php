20e7a6426a17cca3f69212d29a2a14ecb4142aff
Mike Holm <mholm815@gmail.com>
2013-05-04 16:45:02 -0500

    touch up banner text

--- banner.php
+++ banner.php
@@ -1,2 +1,3 @@
 <img class="banner" src="images/pen_ladyslipper_spgc_133x250.jpg" alt="Pen&Ink drawing of a Ladyslipper" />
-<h1 class="banner">Saint Paul Garden Club</h1>
+<div class="banner">Saint Paul</div>
+<div class="banner">Garden Club</div>
