29e1baea117dfa8202b9b56993ee2a374128c657
Mike Holm <mholm815@gmail.com>
2013-05-05 01:31:34 -0500

    disable public navigation until i get those pages build out

--- slide.php
+++ slide.php
@@ -1,6 +1,5 @@
 <div class="stripe">
   <div class="center slide middle">
-    <?php include 'nav.php';?>
     <img class="slide middle" src="images/dreamstime_ladyslippers/width/740.jpg" />
   </div>
 </div>
