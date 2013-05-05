0c6c90a7276db48038863de2e67954e225c233fa
Mike Holm <mholm815@gmail.com>
2013-05-05 15:32:13 -0500

    pull out password checking into functions

--- head.php
+++ head.php
@@ -17,4 +17,25 @@ function loggedIn() {
 function redirect($page, $delay) {
     echo "<meta http-equiv='refresh' content='".$delay.";".$page."' />";
 }
+
+function loginAttempted() {
+    return !empty($_POST['password']);
+}
+
+function tryPassword() {
+    require 'passhash.php';
+
+    $file = fopen("passfile", "r") or exit("can't open file");
+    $storedPassHash = fgets($file);
+    fclose($file);
+
+    if (PassHash::check_password($storedPassHash, $_POST['password'])) {  
+       $_SESSION['LoggedIn'] = 1;
+       $_SESSION['LoginFailed'] = 0;
+    } else {  
+       $_SESSION['LoginFailed'] = 1;
+       $_SESSION['LoggedIn'] = 0;
+    }
+    redirect('index.php', .5);
+}
 ?>
--- index.php
+++ index.php
@@ -2,21 +2,8 @@
 
      if (loggedIn()) {
          redirect('directory.php', 0);
-     } elseif (!empty($_POST['password'])) {
-         require 'passhash.php';
-
-         $file = fopen("passfile", "r") or exit("can't open file");
-         $storedPassHash = fgets($file);
-         fclose($file);
-
-         if (PassHash::check_password($storedPassHash, $_POST['password'])) {  
-             $_SESSION['LoggedIn'] = 1;
-	     $_SESSION['LoginFailed'] = 0;
-         } else {  
-             $_SESSION['LoginFailed'] = 1;
-	     $_SESSION['LoggedIn'] = 0;
-         }
-         redirect('index.php', .5);
+     } elseif (loginAttempted()) {
+         tryPassword();
      } else {
          include 'login.php';
          include 'banner.php';
