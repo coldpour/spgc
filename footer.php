48bb95edd00f140e59109207543d9af84f85974d
Mike Holm <mholm815@gmail.com>
2013-05-07 21:30:01 -0500

    fix footer layout for mac browsers

--- footer.php
+++ footer.php
@@ -3,8 +3,8 @@
 <div class="footer">
      <div class="middle center">
           <div id="copyright" class="foot-content">Copyright 2013</div>
-	  <img class="foot-content" src="images/gca_logo/width/100.jpg" alt="Member of Garden Clubs of America" />
 	  <div id="credit" class="foot-content">Developed by Mike Holm</div>
+	  <img class="foot-content" src="images/gca_logo/width/100.jpg" alt="Member of Garden Clubs of America" />
      </div>
 </div>
 
