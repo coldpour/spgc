b6ae11cda1487eb2a9bbd6ff451e6b2e2c8ea1cb
Mike Holm <mholm815@gmail.com>
2013-05-07 23:48:40 -0500

    structure and style the projects page

--- projects.php
+++ projects.php
@@ -1,59 +1,79 @@
 <?php include 'head.php'; publicPage('projects.php'); ?>
 
-ORGANIZATION
-
-Children's Hospital
-
-Community Design Center
-
-Dayton's Bluff
-
-Hamline Midway Coalition
-
-Lower Phalen Creek Projects
-
-Minnesota State Horticultural Society
-
-Ordway-Flint Hills Festival
-
-St. Paul Parks and Rec
-
-New Rice Park Ad Hoc Committee
-
-New Metropolitan State University
-
-GCA Scholarship
-
-MN Landscape Arboretum
-
-TOTALS
-
-GRANT
-
-$ 2,000.00
-
-$ 2,800.00
-
-$ 1,500.00
-
-$ 1,450.00
-
-$ 2,575.00
-
-$ 3,000.00
-
-$ 1,000.00
-
-$ 3,225.00
-
-$ 4,000.00
-
-$ 1,000.00
-
-$ 250.00
-
-$ 500.00
-
-$ 23,300.00
+<div id='projects' class='slide'>
+  <table class='projects center'>
+    <tbody>
+      <tr class='header'>
+        <th>ORGANIZATION</th>
+        <th>GRANT</th>
+      </t>
+  
+      <tr class='project'>
+        <td>Children's Hospital</td>
+        <td class='amount'>$ 2,000.00</td>
+      </tr>
+  
+      <tr class='project'>
+        <td>Community Design Center</td>
+        <td class='amount'>$ 2,800.00</td>
+      </tr>
+  
+      <tr class='project'>
+        <td>Dayton's Bluff</td>
+        <td class='amount'>$ 1,500.00</td>
+      </tr>
+  
+      <tr class='project'>
+        <td>Hamline Midway Coalition</td>
+        <td class='amount'>$ 1,450.00</td>
+      </tr>
+  
+      <tr class='project'>
+        <td>Lower Phalen Creek Projects</td>
+        <td class='amount'>$ 2,575.00</td>
+      </tr>
+  
+      <tr class='project'>
+        <td>Minnesota State Horticultural Society</td>
+        <td class='amount'>$ 3,000.00</td>
+      </tr>
+  
+      <tr class='project'>
+        <td>Ordway-Flint Hills Festival</td>
+        <td class='amount'>$ 1,000.00</td>
+      </tr>
+  
+      <tr class='project'>
+        <td>St. Paul Parks and Rec</td>
+        <td class='amount'>$ 3,225.00</td>
+      </tr>
+  
+      <tr class='project'>
+        <td>New Rice Park Ad Hoc Committee</td>
+        <td class='amount'>$ 4,000.00</td>
+      </tr>
+  
+      <tr class='project'>
+        <td>New Metropolitan State University</td>
+        <td class='amount'>$ 1,000.00</td>
+      </tr>
+  
+      <tr class='project'>
+        <td>GCA Scholarship</td>
+        <td class='amount'>$ 250.00</td>
+      </tr>
+  
+      <tr class='project'>
+        <td>MN Landscape Arboretum</td>
+        <td class='amount'>$ 500.00</td>
+      </tr>
+  
+      <tr class='total'>
+        <td>TOTALS</td>
+        <td class='amount'>$ 23,300.00</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 
 <?php include 'footer.php'; ?>
