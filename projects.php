7844496264e7730c1b07d6b91f8226140f5da261
Mike Holm <mholm815@gmail.com>
2013-05-07 22:57:56 -0500

    preliminary projects page

---
+++ projects.php
@@ -0,0 +1,59 @@
+<?php include 'head.php'; publicPage('projects.php'); ?>
+
+ORGANIZATION
+
+Children's Hospital
+
+Community Design Center
+
+Dayton's Bluff
+
+Hamline Midway Coalition
+
+Lower Phalen Creek Projects
+
+Minnesota State Horticultural Society
+
+Ordway-Flint Hills Festival
+
+St. Paul Parks and Rec
+
+New Rice Park Ad Hoc Committee
+
+New Metropolitan State University
+
+GCA Scholarship
+
+MN Landscape Arboretum
+
+TOTALS
+
+GRANT
+
+$ 2,000.00
+
+$ 2,800.00
+
+$ 1,500.00
+
+$ 1,450.00
+
+$ 2,575.00
+
+$ 3,000.00
+
+$ 1,000.00
+
+$ 3,225.00
+
+$ 4,000.00
+
+$ 1,000.00
+
+$ 250.00
+
+$ 500.00
+
+$ 23,300.00
+
+<?php include 'footer.php'; ?>
