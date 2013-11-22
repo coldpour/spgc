5d4b4378615eac824084bfcd3f5e20e5caec77e3
Mike Holm <mholm815@gmail.com>
2013-11-22 00:53:20 -0600

    put links in data structure

--- links.php
+++ links.php
@@ -2,15 +2,24 @@
 
 <div id='links' class='slide fixed-height'>
   <ul class='links'>
-    <li class='links'>
-      <a href='http://gcamerica.org'>Garden Club of America</a>
-    </li>
-    <li class='links'>
-      <a href='http://www.northerngardener.org'>Minnesota State Horticultural Society</a>
-    </li>
-    <li class='links'>
-      <a href='http://www.arboretum.umn.edu'>Minnesota Landscape Arboretum</a>
-    </li>
+
+  <?php 
+    $sites = array(
+    	       array('url' => 'http://gcamerica.org', 
+	       	     'text' => 'Garden Club of America'),
+    	       array('url' => 'http://www.northerngardener.org', 
+	       	     'text' => 'Minnesota State Horticultural Society'),
+    	       array('url' => 'http://www.arboretum.umn.edu',
+	             'text' => 'Minnesota Landscape Arboretum'),
+	     );
+
+    for($i = 0; $i < count($sites); ++$i) {
+      echo "<li class='links'>";
+      echo "  <a href=".$sites[$i]['url'].">".$sites[$i]['text']."</a>";
+      echo "</li>";
+    }
+  ?>
+
   </ul>
 </div>
 
