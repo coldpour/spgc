3e57b9d75276d263e502f3d166d0b4ecc8a5c5ec
Mike Holm <mholm815@gmail.com>
2013-12-01 22:38:47 -0600

    create a working order form

--- order.php
+++ order.php
@@ -1,5 +1,7 @@
 <?php include 'head.php'; publicPage('order.php'); ?>
 
+<div id='order' class='slide'>
+
 <?php
 if (isset($_REQUEST['email']))
 //if "email" is filled out, send email
@@ -11,7 +13,7 @@ if (isset($_REQUEST['email']))
   	     "\nAddress: ".$_REQUEST['address1'].
 	     "\nCity, State, Zip Code: ".$_REQUEST['address2'].
 	     "\nE-mail: ".$_REQUEST['email'].
-	     "\nTel: ".$_REQUEST['tel'];
+	     "\nPhone: ".$_REQUEST['phone'];
   mail("mholm815@gmail.com", $subject,
   $message, "From:" . $email);
   echo "Thank you for using our mail form";
@@ -19,17 +21,30 @@ if (isset($_REQUEST['email']))
 else
 //if "email" is not filled out, display the form
   {
-  echo "<div id='order' class='slide'>
-    <form method='post' action='order.php'>
-      Name: <input type='text' name='name'><br />
-      Address: <input type='text' name='address1'><br />
-      City, State, Zip Code: <input type='text' name='address2'><br />
-      E-mail: <input type='text' name='email'>
-      Tel. <input type='text' name='tel'><br />
-      <input type='submit' value='Submit'>
-    </form>
-  </div>";
+  echo "
+    <form id='orderform' class='center' method='post' action='order.php'>
+      <div class='order'>
+        Name: <input class='order' type='text' name='name'>
+      </div>
+      <div class='order'>
+        Address: <input class='order' type='text' name='address1'>
+      </div>
+      <div class='order'>
+        City, State, Zip: <input class='order' type='text' name='address2'>
+      </div>
+      <div class='order'>
+        E-mail: <input class='order' type='text' name='email'>
+      </div>
+      <div class='order'>
+        Phone: <input class='order' type='text' name='phone'>
+      </div>
+      <div class='order'>
+        <input class='order' type='submit' value='Submit'>
+      </div>
+    </form>";
   }
 ?>
+
+</div>
 
 <?php include 'footer.php'; ?>
